0de3788d936b3fe5bdbcb61fc4e591be7d853bc9
Josh Flayhart <ijoshg8@mac.com>
2017-12-26 09:56:19 -0600

    add mui theme styles

---
+++ citysensor/web/styles/muiTheme.js
@@ -0,0 +1,23 @@
+// https://material-ui-1dab0.firebaseapp.com/style/color
+export default {
+  palette: {
+    type: 'light',
+    primary: {
+      50: '#e3f2fd',
+      100: '#bbdefb',
+      200: '#90caf9',
+      300: '#64b5f6',
+      400: '#42a5f5',
+      500: '#2196f3', // buttons and other primary
+      600: '#1e88e5',
+      700: '#1976d2',
+      800: '#1565c0',
+      900: '#0d47a1',
+      A100: '#82b1ff',
+      A200: '#448aff',
+      A400: '#2979ff',
+      A700: '#2962ff',
+      contrastDefaultColor: 'light',
+    },
+  },
+};
