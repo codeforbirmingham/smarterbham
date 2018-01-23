bad854ea300aebe570b5d1f8c1466be4dbc810ae
Josh Flayhart <ijoshg8@mac.com>
2018-01-23 16:26:37 -0600

    improve dist script for packaging app

--- citysensor/scripts/pkgApp.js
+++ citysensor/scripts/pkgApp.js
@@ -1,3 +1,3 @@
-// when pkg-ing the app we set NODE_ENV as production and require the `./server.js`
+// when pkg-ing the app we set NODE_ENV as production and require src/server.js
 process.env.NODE_ENV = 'production';
 require('../src/server.js');
